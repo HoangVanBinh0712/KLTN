a34d62eef509229b1bedc95edfafce6a400ea6c2
HoangVanBinh0712 <thebest11447@gmail.com>
2023-05-27 19:30:02 +0700

    fix css and add post

--- Frontend/client/src/employer_scenes/components/AddPostComponent.js
+++ Frontend/client/src/employer_scenes/components/AddPostComponent.js
@@ -298,7 +298,7 @@ const AddPostComponent = () => {
                     type="radio"
                     id="currency1"
                     name="currency"
-                    value="USD"
+                    value="VND"
                     style={{ width: '15%' }}
                     defaultChecked
                     onChange={onChangeCurencyType}
